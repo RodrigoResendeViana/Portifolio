4a0f7466810b7e46bc029362323a4bb28887395c
Rodrigo Viana <digo1209@gmail.com>
2024-08-19 22:36:28 -0300

    Header finalizado, iniciando about

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
 import curriculo from '../../assets/RodrigoResendeViana_Curriculo_organized.pdf'
+import avatar from '../../assets/avatar_about.png'
 
 import './About.css'
 
 const About = () => {
   return (
     <div className='portifolio__about' id='about'>
-      <a href={curriculo} download="curriculo-Rodrigo_Viana.pdf">
-        <button type='button'>Download Resumé</button>
-      </a>
+      <div className="portifolio__about-infos">
+        <h1>Sobre mim</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea libero quia corporis magnam deserunt enim pariatur eaque. Quibusdam doloribus est temporibus ex, aspernatur nemo, optio eligendi cumque, vel facere blanditiis.</p>
+        <a href={curriculo} download="curriculo-Rodrigo_Viana.pdf">
+          <button type='button'>Download Resumé</button>
+        </a>
+      </div>
+      <img src={avatar} alt="" />
     </div>
   )
 }
